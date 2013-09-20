939525b5bd6a4a76be3c240294d1707688e92684
Morgan 'ARR\!' Allen <morganrallen@gmail.com>
2013-09-20 11:08:45 -0700

    pages

---
+++ test.js
@@ -0,0 +1,64 @@
+var tape = this.tape;
+var _ = this._;
+var Backbone = this.Backbone;
+
+if(!tape && (typeof require !== "undefined")) tape = require("tape");
+if(!_ && (typeof require !== "undefined")) _ = require("underscore");
+if(!Backbone && (typeof require !== "undefined")) Backbone = require("./");
+
+var planetExpressConfig = {
+  location: "New New York",
+  ship: "Planet Express Ship"
+};
+
+var planetExpressMembers = [ "Fry", "Lela", "Zoidberg" ];
+
+var DOOPConfig = {
+  location: "New New Jersey"
+};
+
+var DOOPMembers = [ "Zaap", "Kif", "Hugh Man" ];
+
+var members = [];
+
+_(planetExpressMembers).forEach(function(member) {
+  members.push(_.extend({}, planetExpressConfig, { name: member }));
+});
+
+_(DOOPMembers).forEach(function(member) {
+  members.push(_.extend({}, DOOPConfig, { name: member }));
+});
+
+var c = new Backbone.Collection(members);
+
+tape("Check main Collection", function(t) {
+  t.plan(2);
+  t.ok(c.filterWhere);
+  t.equal(c.length, 6, "members.length");
+});
+
+var planetExpress = c.filterWhere({ ship: planetExpressConfig.ship });
+
+tape("Create filter", function(t) {
+  t.plan(1);
+  t.equal(planetExpress.length, 3, "planetExpress.length");
+});
+
+tape("Add filterable item to main Collection", function(t) {
+  t.plan(2);
+
+  c.add([
+    _.extend({}, planetExpressConfig, { name: "Bender" })
+  ]);
+
+  t.equal(c.length, 7, "members.length");
+  t.equal(planetExpress.length, 4, "planetExpress.length");
+});
+
+tape("Remove filterable item from main Collection", function(t) {
+  t.plan(2);
+  c.remove(c.at(0));
+
+  t.equal(c.length, 6, "members.length");
+  t.equal(planetExpress.length, 3, "planetExpress.length");
+});
